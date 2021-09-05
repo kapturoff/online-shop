04a6c17262758893227e9322894be77aaf66ae22
kapturoff <pigunderwater@gmail.com>
2021-09-06 01:56:40 +0300

    Edit some field names and behavior of models
    I edited some variable names (for example: "datetime_created" to "created") because information in these fields was a bit unnecessary as
    I see now. Then I edited serializers of orders to make them more flexible in work with related fields. Afterwards, I created the .create() method
    for serializers. There's a real need in redefining it, because this is the only way in Django Rest Framework and Django to create needed
    kind of relationship between models.

--- order_data_storage/models.py
+++ order_data_storage/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from users_data_storage.models import User
+from django.contrib.auth.models import User
 from products_data_storage.models import Product
 
-# Create your models here.
+
+DEFAULT_ORDER_STATUS_ID = 1 # ID of status "Created"
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=20)
@@ -15,10 +16,10 @@ class Order(models.Model):
         on_delete=models.SET_NULL,
     )
     final_cost = models.FloatField()
-    datetime_created = models.DateTimeField(auto_now=True)
-    status = models.ForeignKey(OrderStatus, on_delete=models.RESTRICT)
+    created = models.DateTimeField(auto_now=True)
+    status = models.ForeignKey(OrderStatus, default=DEFAULT_ORDER_STATUS_ID, on_delete=models.RESTRICT)
 
-    # Create seperated model for addresses
+    # TODO: Create seperated model for addresses
     address_to_send = models.CharField(max_length=100)
     email = models.CharField(max_length=50)
     first_name = models.CharField(max_length=20)
@@ -26,16 +27,16 @@ class Order(models.Model):
     mobile_number = models.CharField(max_length=20)
 
     def __str__(self) -> str:
-        return f'{self.email}, ${self.datetime_created}'
+        return f'{self.customer} {self.created}'
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.RESTRICT)
-    bought_amount = models.PositiveSmallIntegerField()
+    amount = models.PositiveSmallIntegerField()
 
     def __str__(self) -> str:
-        return f'{self.product.name} | {self.order}'
+        return f'{self.product.name}, {self.order}'
 
 
 class Transaction(models.Model):
--- order_data_storage/serializers.py
+++ order_data_storage/serializers.py
@@ -1,3 +1,4 @@
+from products_data_storage.models import Product
 from users_data_storage.serializers import UserSerializer
 from products_data_storage.serializers import ProductSerializer
 from rest_framework import serializers
@@ -7,27 +8,72 @@ from order_data_storage import models
 class OrderStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OrderStatus
-        fields = "__all__"
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.Serializer):
+    product = ProductSerializer()
+    amount = serializers.IntegerField(max_value=32767, min_value=0)
 
 
 class OrderSerializer(serializers.ModelSerializer):
     customer = UserSerializer()
-    status = OrderStatusSerializer()
+    status = OrderStatusSerializer(default=1)
+    items = OrderItemSerializer(many=True)
+
+    def create(data, customer):
+        # It may seem like we shouldn't make another iteration on array for getting products from
+        # database again, because here we expect to see an already prepared list of items that can 
+        # be borrowed from cart of an user (and this kind of has a point because CartItem and OrderItem 
+        # models have almost identical fields).
+        #
+        # But the truth is that the request could contain IDs that're no longer existing
+        # (or even IDs that never been existing), so we need to check every item from list for existing.
+        # And we actually want to summarize their costs to get final cost of order, so we can use this 
+        # iteration to kill two birds with one stone.
+        raw_items = data['items']
+        items = []
+        final_cost = 0.0
+
+        for item in raw_items:
+            product = Product.objects.get(id=item['product']['id'])
+            final_cost += product.price
+            items.append({'product': product, 'amount': item['amount']})
+
+        if not len(raw_items): raise IndexError('Field \'items\' must contain at least one product.')
+
+        order = models.Order(
+            customer=customer,
+            final_cost=final_cost, 
+            address_to_send=data['address_to_send'],
+            email=data['email'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            mobile_number=data['mobile_number'],
+        )
+
+        order.save()
+
+        # And now we need to iterate items one more time to add them to order.
+        # We could not make it before, because we did not create any order, but we was still in
+        # need of first iteration because of we were summarizing final cost of whole order (order
+        # cannot be created without final_cost field)
+        #
+        # TODO: Rewrite final cost field as getter in Order model to avoid it.
+        for item in items:
+            order_item = models.OrderItem(product=product, amount=item['amount'], order=order)
+            order_item.save()
+
+        return order
 
     class Meta:
         model = models.Order
-        fields = "__all__"
-
-
-class OrderItemSerializer(serializers.Serializer):
-    order = OrderSerializer()
-    product = ProductSerializer()
-    bought_amount = serializers.IntegerField(max_value=32767, min_value=0)
-
+        fields = '__all__'
+        
 
 class TransactionSerializer(serializers.ModelSerializer):
     order = OrderSerializer()
 
     class Meta:
         model = models.Transaction
-        fields = "__any__"
+        fields = '__any__'
