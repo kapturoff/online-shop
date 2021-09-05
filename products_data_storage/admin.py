6eedfdac838c091cd3fff59c3171fb85e113515d
kapturoff <pigunderwater@gmail.com>
2021-09-06 02:32:53 +0300

    Register models in Django admin site

--- products_data_storage/admin.py
+++ products_data_storage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class ProductAdmin(admin.TabularInline):
+    model = Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [ProductAdmin]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
