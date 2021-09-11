e22b242ca13c093492661cd029713569b102be68
kapturoff <pigunderwater@gmail.com>
2021-09-11 19:19:45 +0300

    Fix a bug with incorrect comparison in permission
    Before this fix, permission was comparing ID of an order with ID of an user. Now it compares the customer ID of
    an order and user ID of an user that requested the endpoint.

--- order_data_storage/permissions.py
+++ order_data_storage/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import BasePermission
+from .models import Order
 
-class IsOwnerOrAdmin(BasePermission):
+class IsOrderOwnerOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        # return super().has_permission(request, view)
-        return bool(view.kwargs['order_id'] is request.user.id) or bool(request.user.is_staff)
+        try: 
+            order_id = view.kwargs['order_id']
+            order = Order.objects.get(id=order_id)
+            return bool(order.customer.id is request.user.id) or bool(request.user.is_staff)
+        except (Order.DoesNotExist, KeyError):
+            return False
