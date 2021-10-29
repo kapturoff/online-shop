59b5e1ff8fe10a0987b5d30f0edf5366dc851ebf
kapturoff <pigunderwater@gmail.com>
2021-10-29 19:18:48 +0300

    Restore the previous behavior of OrderSerializer.create()
    I found that requesting the database in the loop is needed behavior since we must ensure that every product that an user asks is existing.
    Previous approach is just filtering the IDs and does not throw any error if an user passed a product that is not the database, so I've
    decided to return the database requests in the loop.

--- orders/serializers.py
+++ orders/serializers.py
@@ -35,32 +35,36 @@ class OrderSerializer(serializers.ModelSerializer):
         use this iteration to kill two birds with one stone.
         '''
 
-        # If list of items for the new order is empty, throw an error.
-        if not len(data['items']):
-            raise ValidationError(
-                'Field \'items\' must contain at least one product.'
-            )
-
+        raw_items = data['items']
         items = []
-        items_IDs = list(map(lambda item: item['product']['id'], data['items']))
-        products = Product.objects.filter(id__in=items_IDs)
         final_cost = 0.0
 
-        for product in products:
-            # Finding back the item user sent for combinging it with the product in the database.
-            item = next(
-                x for x in data['items'] if x['product']['id'] == product.id
-            )
+        for item in raw_items:
+            '''
+            Making the database requests in a loop is the bad tone and it's bad for perfomance, but 
+            this behaviour is needed because we need to ensure that every product that user wants to buy exists and we
+            can achieve it by checking products manually, request by request.
 
-            # Here we must check that the user cannot buy more products than is available in the database.
+            The alternative of this is using an one SQL request which may look like:
+            SELECT . from products WHERE id IN [1, 2, ...raw items IDs]
+            But this approach does not throw any error if the user passed the product that does not
+            exist as it simply filters given IDs and skips those ones that are not in the database.
+            '''
+            product = Product.objects.get(id=item['product']['id'])
+
+            # Here we must check that an user cannot buy more products than is available in the database.
             if (product.amount_remaining < item['amount']):
                 raise ValidationError(
                     f'The quantity of product with ID {product.id} is not enough to add this amount to the order.'
                 )
 
-            # Since the product price is DecimalField it must be explicitly converted
             final_cost += round(float(product.price) * item['amount'], 2)
             items.append({'product': product, 'amount': item['amount']})
+
+        if not len(raw_items):
+            raise ValidationError(
+                'Field \'items\' must contain at least one product.'
+            )
 
         order = models.Order(
             customer=customer,
@@ -79,7 +83,7 @@ class OrderSerializer(serializers.ModelSerializer):
         need of first iteration because of we were summarizing final cost of whole order (and order
         cannot be created without final_cost field)
         
-        TODO: Rewrite final cost field as getter in Order model to avoid it (#e3eae90a)
+        TODO: Rewrite final cost field as getter in Order model to avoid it. (#e3eae90a)
         '''
         for item in items:
             order_item = models.OrderItem(
