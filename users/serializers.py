db444a3a9ed0288c9e986df82a13fe2db7e02ea3
kapturoff <pigunderwater@gmail.com>
2021-11-26 23:53:33 +0300

    Edit RegisterSerializer to validate the uniqueness
    I slowly add the token authorization to the project and because the DRF implementation of it is strongly related with Django User model, it
    throws ugly exceptions when user provide an not unique username and email. With DRF serializer validation mechanism these exceptions
    become more human-readable, so I put the validators here.

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from products.serializers import ProductSerializer
-from . import models
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
@@ -19,9 +19,35 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'date_joined', 'last_login']
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, required=True)
     email = serializers.CharField(required=True)
+    date_joined = serializers.DateTimeField(read_only=True)
+    last_login = serializers.DateTimeField(read_only=True)
+
+    def validate_email(self, value):
+        """
+        Check that the email is unique
+        """
+        email_is_used = User.objects.filter(email=value)
+
+        if email_is_used:
+            raise serializers.ValidationError('The email is already taken')
+
+        return value
+
+    def validate_username(self, value):
+        """
+        Check that the username is unique
+        """
+        username_is_used = User.objects.filter(username=value)
+
+        if username_is_used:
+            raise serializers.ValidationError('The username is already taken')
+
+        return value
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
@@ -29,9 +55,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
-
-    class Meta:
-        model = User
-        fields = [
-            'id', 'username', 'date_joined', 'last_login', 'password', 'email'
-        ]
