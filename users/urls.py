d9025ddf2c35d6d801dd4e7d12a221af0f85540a
kapturoff <pigunderwater@gmail.com>
2021-11-26 23:58:40 +0300

    Add obtain_auth_token from DRF (#1603b2ba)
    I intented to make the token authorization optional, so if user ever needs the token, he can create or receieve an existing one by
    visiting '/token' endpoint, putting in the request body his username and password

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
-
 
 urlpatterns = [
     path('register', views.UserRegister.as_view()),
+    path('token', obtain_auth_token, name='api_token_auth'),
     path('users/<int:user_id>', views.UserDetail.as_view()),
     path('users/<int:user_id>/wishlist', views.Wishlist.as_view()),
     path('users/<int:user_id>/cart', views.Cart.as_view()),
