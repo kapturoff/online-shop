cb77e45ecf08729c8545a8ca96ca95f364938210
kapturoff <pigunderwater@gmail.com>
2021-12-25 19:27:57 +0300

    Add tests for reviews (#01b6b1d0)

--- products/tests.py
+++ products/tests.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Product, Category
-from .serializers import CategorySerializer, ProductListSerializer, ProductSerializer
+from django.contrib.auth.models import User
+from .models import Product, Category, Review
+from .serializers import CategorySerializer, ProductListSerializer, ProductSerializer, ReviewSerializer
 
 test_data = {
     'category 1': {
@@ -30,6 +31,26 @@ test_data = {
             'size': 'S',
             'color': 'yellow',
         },
+    'review 1': {
+        'liked': True,
+        'review_text': 'Awesome t-shirt!'
+    },
+    'review 2': {
+        'liked': False,
+        'review_text': 'I hate this t-shirt!'
+    },
+    'review without liked field': {
+        'review_text': 'Awesome t-shirt!'
+    },
+    'review without caption': {
+        'liked': True,
+    },
+    'user':
+        {
+            'username': 'john',
+            'password': 'johnsstrongpassword',
+            'email': 'john@jonny.jo'
+        }
 }
 
 
@@ -96,6 +117,15 @@ class ProductsTest(APITestCase):
 
         self.assertEquals(response.data, expected_result.data)
 
+    def test_get_second_product(self):
+        '''
+        Make sure that we get other result when we asks for another product details
+        '''
+        expected_result = ProductSerializer(self.p2)
+        response = self.client.get('/categories/top%20clothes/2')
+
+        self.assertEquals(response.data, expected_result.data)
+
     def test_product_has_fields(self):
         '''
         Make sure that a returned product has all important fields
@@ -106,10 +136,14 @@ class ProductsTest(APITestCase):
         self.assertEqual(data['name'], test_data['product 1']['name'])
         self.assertEqual(data['img'], test_data['product 1']['img'])
         self.assertEqual(data['price'], str(test_data['product 1']['price']))
-        self.assertEqual(data['amount_remaining'], test_data['product 1']['amount_remaining'])
+        self.assertEqual(
+            data['amount_remaining'], test_data['product 1']['amount_remaining']
+        )
         self.assertEqual(data['size'], test_data['product 1']['size'])
         self.assertEqual(data['color'], test_data['product 1']['color'])
-        self.assertEqual(data['description'], test_data['product 1']['description'])
+        self.assertEqual(
+            data['description'], test_data['product 1']['description']
+        )
 
     def test_get_product_that_does_not_exist(self):
         '''
@@ -119,3 +153,236 @@ class ProductsTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(response.data['detail'].code, 'not_found')
+
+
+class ReviewListTest(APITestCase):
+    def setUp(self) -> None:
+        self.c1 = Category(**test_data['category 1'])
+        self.c2 = Category(**test_data['category 2'])
+        self.c1.save()
+        self.c2.save()
+
+        self.p1 = Product(**test_data['product 1'], category=self.c1)
+        self.p2 = Product(**test_data['product 2'], category=self.c1)
+        self.p1.save()
+        self.p2.save()
+
+        self.user = User.objects.create_user(**test_data['user'])
+        self.user.save()
+
+        self.r1 = Review(
+            **test_data['review 1'], author=self.user, product=self.p1
+        )
+        self.r2 = Review(
+            **test_data['review 2'], author=self.user, product=self.p1
+        )
+        self.r1.save()
+        self.r2.save()
+
+    def test_get_review_list(self):
+        '''
+        Make sure that API returns list of reviews
+        '''
+        expected_result = ReviewSerializer([self.r1, self.r2], many=True)
+        response = self.client.get('/categories/top%20clothes/1/reviews')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_result.data)
+
+    def test_get_empty_review_list(self):
+        '''
+        API returns empty list if there are no reviews below product
+        '''
+        expected_result = ReviewSerializer([], many=True)
+        response = self.client.get('/categories/top%20clothes/2/reviews')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_result.data)
+
+    def test_get_review_invalid_id(self):
+        '''
+        Ensure that API throws an error if we're trying to get list of reviews from product that does not exist
+        '''
+        response = self.client.get('/categories/top%20clothes/999999/reviews')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['detail'].code, 'not_found')
+
+
+class ReviewCreateTest(APITestCase):
+    def setUp(self) -> None:
+        self.c1 = Category(**test_data['category 1'])
+        self.c1.save()
+
+        self.p1 = Product(**test_data['product 1'], category=self.c1)
+        self.p1.save()
+
+        self.user = User.objects.create_user(**test_data['user'])
+        self.user.save()
+
+        self.user_token = 'Token ' + self.client.post(
+            '/token', test_data['user'], format='json'
+        ).data['token']
+
+    '''
+    [x] product_id, logged in, liked, review_text
+    [x] product_id, logged in, liked, review_text (x2)
+    [x] product_id is invalid, logged in, liked, review_text
+    [] product_id is valid, logged out, liked, review_text
+    [x] product_id is valid, logged in, no liked, review_text
+    [] product_id, logged in, liked, no review_text
+    '''
+
+    def test_create_review(self):
+        '''
+        Ensure that we can create the new review if we logged in, pass valid 
+        product ID and provide fields "liked" and "review_text".
+        '''
+        review = Review(
+            **test_data['review 1'], author=self.user, product=self.p1
+        )  # Not saving!
+
+        # Because the ID is not being set automatically
+        # if we don't save model to the database, we must set it manually
+        review.id = 1
+        expected_result = ReviewSerializer(review)
+        response = self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review 1'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+
+        self.assertEqual(expected_result.data, response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Get review from database after request
+        saved_review = Review.objects.get(id=1)
+
+        # Test that the review is now saved in database
+        self.assertTrue(saved_review)
+
+    def test_create_review_twice(self):
+        '''
+        Ensure that we can create two new reviews in a row if we logged in, pass valid 
+        product IDs and provide fields "liked" and "review_text".
+        '''
+        self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review 1'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+        self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review 2'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+
+        # Get reviews from database after requests
+        saved_review_1 = Review.objects.get(id=1)
+        saved_review_2 = Review.objects.get(id=2)
+
+        # Test that the reviews are now saved in database
+        self.assertTrue(saved_review_1)
+        self.assertTrue(saved_review_2)
+
+    def test_create_review_invalid_id(self):
+        '''
+        Test that we cannot create new review for product that does not exist
+        '''
+        response = self.client.post(
+            '/categories/top%20clothes/999999999/reviews/create',
+            test_data['review 1'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['detail'].code, 'not_found')
+
+        # Get review if any of them exist
+        review = Review.objects.filter(id=1).first()
+
+        # Test that nothing was saved in database
+        self.assertFalse(review)
+
+    def test_create_review_no_liked(self):
+        '''
+        Test that we cannot create new review without providing "liked" field
+        '''
+        response = self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review without liked field'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data[0].code, 'invalid')
+
+        # Get review if any of them exist
+        review = Review.objects.filter(id=1).first()
+
+        # Test that nothing was saved in database
+        self.assertFalse(review)
+
+    def test_create_review_no_liked(self):
+        '''
+        Test that we cannot create new review without providing "liked" field
+        '''
+        response = self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review without liked field'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data[0].code, 'invalid')
+
+        # Get review if any of them exist
+        review = Review.objects.filter(id=1).first()
+
+        # Test that nothing was saved in database
+        self.assertFalse(review)
+
+    def test_create_review_no_review_text(self):
+        '''
+        Test that we cannot create new review without providing "review_text" field
+        '''
+        response = self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review without caption'],
+            format='json',
+            HTTP_AUTHORIZATION=self.user_token
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data[0].code, 'invalid')
+
+        # Get review if any of them exist
+        review = Review.objects.filter(id=1).first()
+
+        # Test that nothing was saved in database
+        self.assertFalse(review)
+
+    def test_create_review_logged_out(self):
+        '''
+        Test that we cannot create new review without authorization
+        '''
+        response = self.client.post(
+            '/categories/top%20clothes/1/reviews/create',
+            test_data['review 1'],
+            format='json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'].code, 'not_authenticated')
+
+        # Get review if any of them exist
+        review = Review.objects.filter(id=1).first()
+
+        # Test that nothing was saved in database
+        self.assertFalse(review)
