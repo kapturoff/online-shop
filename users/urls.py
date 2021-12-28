727ed9733a8b197475246bcbaf4c6e98d5cb3df7
kapturoff <pigunderwater@gmail.com>
2021-12-28 22:39:29 +0300

    Delete unnecessary word in endpoint

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('token', obtain_auth_token, name='api_token_auth'),
     path('users/<int:user_id>', views.UserDetail.as_view()),
     path('users/<int:user_id>/wishlist', views.Wishlist.as_view()),
-    path('users/<int:user_id>/wishlist/<int:wishlist_item_id>/delete', views.WishlistItemDelete.as_view()),
+    path('users/<int:user_id>/wishlist/<int:wishlist_item_id>', views.WishlistItemDelete.as_view()),
     path('users/<int:user_id>/cart', views.Cart.as_view()),
-    path('users/<int:user_id>/cart/<int:cart_item_id>/delete', views.CartItemDelete.as_view()),
+    path('users/<int:user_id>/cart/<int:cart_item_id>', views.CartItemDelete.as_view()),
     path('users/<int:user_id>/reviews', views.ReviewList.as_view()),
 ]
