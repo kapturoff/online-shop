eb6a216b228b0901b17a8cd42487a8d93cbfef95
kapturoff <pigunderwater@gmail.com>
2021-12-28 23:14:46 +0300

    Edit IsReviewOwner permission behavior
    Now it does not throw any error if it does not find the user in database.

--- users/permissions.py
+++ users/permissions.py
@@ -1,11 +1,15 @@
 from rest_framework.permissions import BasePermission
 from products.models import Review
 
+
 class IsOwner(BasePermission):
     def has_permission(self, request, view):
         return request.user.id == view.kwargs['user_id']
 
+
 class IsReviewOwner(BasePermission):
     def has_permission(self, request, view):
-        review = Review.objects.get(id = view.kwargs['user_id'])
-        return request.user.id == review.author.id
+        all_reviews_of_user = Review.objects.filter(author__id=request.user.id)
+        IDs = [review.id for review in all_reviews_of_user]
+
+        return view.kwargs['review_id'] in IDs
