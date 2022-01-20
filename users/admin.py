092a1af75caecb96c1423795d55d09c09016c24e
kapturoff <pigunderwater@gmail.com>
2022-01-21 01:00:07 +0300

    Edit admin page to show user's reviews count

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'is_staff',
+        'commentaries'
+    )
+
+    def commentaries(self, obj):
+        return obj.commentaries.count()
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
