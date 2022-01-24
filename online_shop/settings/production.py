2c5f70b95d8d3d6627a8fc891ec8f45644a405e6
kapturoff <pigunderwater@gmail.com>
2022-01-25 00:52:16 +0300

    Add a disclaimer

--- online_shop/settings/production.py
+++ online_shop/settings/production.py
@@ -6,4 +6,5 @@ SECRET_KEY = environ['SECRET_KEY']
 
 DEBUG = False
 
+# Security warning: Must be replaced with hostname of server where you will run the project
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
