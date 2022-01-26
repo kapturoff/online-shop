a04651f7b9835d727784cfef5aa9bb914b521323
kapturoff <pigunderwater@gmail.com>
2022-01-26 23:42:01 +0300

    Fix the error with using env variables
    Settings weren't getting any environment variables from anywhere because they were using the environ module of the standart Python library as a function. Now it uses its method.

--- online_shop/settings/production.py
+++ online_shop/settings/production.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = environ['SECRET_KEY']
+SECRET_KEY = environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = [environ('HOSTNAME')]
+ALLOWED_HOSTS = [environ.get('HOSTNAME')]
 
 DATABASES = {
     'default':
         {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': environ('MYSQL_DATABASE_NAME'),
-            'USER': environ('MYSQL_USER'),
-            'PASSWORD': environ('MYSQL_PASSWORD'),
-            'HOST': environ('MYSQL_HOST'),
-            'PORT': environ('MYSQL_PORT'),
+            'NAME': environ.get('MYSQL_DATABASE_NAME'),
+            'USER': environ.get('MYSQL_USER'),
+            'PASSWORD': environ.get('MYSQL_PASSWORD'),
+            'HOST': environ.get('MYSQL_HOST'),
+            'PORT': environ.get('MYSQL_PORT'),
         }
 }
